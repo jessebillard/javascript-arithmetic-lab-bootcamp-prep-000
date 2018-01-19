31540f3ed2e8f7336ffc70b60b2aecb8c40acc70
Jesse Billard <jessebillard@mac.com>
2018-01-19 19:25:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,49 @@
+var a = 10
+var b = 5
+var c = 20
+var d = 15.567
+var e = parseInt('nonsense!', 10)
+var f = parseFloat('2.222')
+var g = 2
+
+function add(a, b) {
+  return (a + b)
+}
+
+function subtract(a, b) {
+  return (a - b)
+}
+
+function multiply(a, b) {
+  return (a * b)
+}
+
+function divide(a, b) {
+  return (a / b)
+}
+
+function inc(c) {
+  return (++c)
+}
+
+function dec(c) {
+  return (--c)
+}
+
+function makeInt(a) {
+  return (parseInt('10', 10))
+}
+
+function makeInt(e) {
+  return parseInt("e");
+}
+
+function preserveDecimal(f) {
+  return (parseFloat('2.222'))
+}
+
+function preserveDecimal(e) {
+  return (parseInt('e'))
+}
+
+
